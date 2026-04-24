4b3e4a8a0d1b6ab9c7463a23b860cc80cb7f4f80
Illia Datsiuk <ilya.datsiuk@gmail.com>
2026-04-24 16:26:22 +0200

    fix: admin ID instead of username in .env

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_USER_ID = os.getenv("ADMIN_USERNAME")
+ADMIN_USER_ID = os.getenv("ADMIN_USER_ID") or os.getenv("ADMIN_USERNAME")
 DATABASE_URL = os.getenv("DATABASE_URL")
 COOKIES_CONTENT = os.getenv("COOKIES_CONTENT")
 WHITELISTED_ENV = os.getenv("WHITELISTED", "")
